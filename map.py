782ee4c94dbb7a328c5df3ff63a4c7c09121be1e
Alisher Kenzhebek uulu <alisher.andakeev@gmail.com>
2023-02-10 16:11:09 +0600

    add map.py

---
+++ map.py
@@ -0,0 +1,21 @@
+# b = map(int, ['1', '2', '3', '4', '5', '6', '7'])
+
+# a = sum(b)
+# a = min(b)
+# a = max(b)
+# a = sum(b) #Дважды пройтись по коллекции нельзя!
+# print(a)
+# print(next(b))
+# print(next(b))
+# for x in b:
+#     print(x, end=" ")
+
+'''----------------------------------
+b = (int(x) for x in ['1', '2', '3', '4', '5', '6', '7'])
+a = list(b)
+print(a)
+----------------------------------'''
+
+# s = map(int, input().split())
+# a = list(s)
+# print(a)
